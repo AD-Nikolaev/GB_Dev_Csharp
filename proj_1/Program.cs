1a910f37d6e48f5cbde6ec4a7c062d43073821a5
AD-Nikolaev <dancer.alexsey@gmail.com>
2023-02-26 16:30:10 +0300

    Сделана задача 2

---
+++ proj_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b) Console.WriteLine($"Число {a} > числа {b}");
+else if (b > a) Console.WriteLine($"Число {b} > числа {a}");
+else Console.WriteLine($"Число {a} = числу {b}");
