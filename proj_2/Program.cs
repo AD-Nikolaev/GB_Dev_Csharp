ca1d4545047bb5774d05e50e8ee14959e53cc89f
AD-Nikolaev <dancer.alexsey@gmail.com>
2023-02-26 16:42:30 +0300

    Сделана задача 4

---
+++ proj_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+
+Console.Write($"Максимальное среди введённых чисел: {Math.Max(a, Math.Max(b, c))}");
