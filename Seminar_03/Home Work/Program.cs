9133a7d7276b12210cb78023ad5de9d5459b564a
AD-Nikolaev <dancer.alexsey@gmail.com>
2023-03-09 19:42:40 +0300

    Выполнена задача № 19

---
+++ Seminar_03/Home Work/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+bool IsNumberPalindrome(int number)
+{
+    int result = 0;
+
+    int tmpNumber = number;
+    while (tmpNumber > 0)
+    {
+        result = result * 10 + tmpNumber % 10;
+        tmpNumber /= 10;
+    }
+
+    if (result == number) return true;
+    else return false;
+}
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (IsNumberPalindrome(number))
+{
+    Console.Write("Введённое число - палиндром.");
+}
+else
+{
+    Console.Write("Введённое число - не палиндром.");
+}
